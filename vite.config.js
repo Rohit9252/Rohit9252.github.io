1ebfdf740ac76fee95f8a1e6802439f9e67952bb
Rohit9252 <rohitbatra0165@gmail.com>
2025-12-29 16:06:42 +0530

    add ci-cd pipline

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: 'Rohit9252.github.io',
+  base: './',  // Use relative paths - works for any deployment
 })
